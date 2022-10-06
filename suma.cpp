72617197b5473808f05c66445ead15dd848e6e67
Wilton3 <julianaorejuela26@gmail.com>
2022-10-06 09:19:46 -0500

    Actividad

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+//Creado por Jeohassin Wilton Orejuela Garcia
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float x,y,j,z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
+ cout<<"El resutado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+}
